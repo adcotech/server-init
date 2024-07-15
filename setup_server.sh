e7f56bb8d29d2c0191f7f695da275c1e41aaa9cd
adcotech <Ad@caribeoriginals.com>
2024-07-15 16:05:25 -0400

    Update setup_server.sh
    just changed the names from code agency to adcotech(whitebranding)

--- setup_server.sh
+++ setup_server.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-# Create new user "codeagency" with SSH key
-useradd -m -s /bin/bash codeagency
-mkdir -p /home/codeagency/.ssh
-cp /root/.ssh/id_ed25519.pub /home/codeagency/.ssh/authorized_keys
-chown -R codeagency:codeagency /home/codeagency/.ssh
+# Create new user "adcotech" with SSH key
+useradd -m -s /bin/bash adcotech
+mkdir -p /home/adcotech/.ssh
+cp /root/.ssh/id_ed25519.pub /home/adcotech/.ssh/authorized_keys
+chown -R adcotech:adcotech /home/adcotech/.ssh
 
-# Add user "codeagency" to sudo and docker groups
-usermod -aG sudo codeagency
-usermod -aG docker codeagency
+# Add user "adcotech" to sudo and docker groups
+usermod -aG sudo adcotech
+usermod -aG docker adcotech
 
 # Update and upgrade packages
 apt-get update -y
@@ -17,20 +17,20 @@ apt-get upgrade -y
 apt-get install -y unattended-upgrades
 dpkg-reconfigure --priority=low unattended-upgrades
 
-echo "Code Agency Cloud is containerizing your server... Please wait a moment."
+echo "Adcotech Cloud is containerizing your server... Please wait a moment."
 # Install Docker and Docker Compose
 curl -fsSL https://get.docker.com -o get-docker.sh
 sh get-docker.sh
-usermod -aG docker codeagency
+usermod -aG docker adcotech
 systemctl enable docker
 systemctl start docker
 apt-get install -y docker-compose
-echo "Code Agency Cloud is installing essential packages on your server... Please wait a moment."
+echo "Adcotech Cloud is installing essential packages on your server... Please wait a moment."
 apt-get install -y build-essential wget curl git
-echo "Code Agency Cloud is installing Homebrew on your server... Please wait a moment."
+echo "Adcotech Cloud is installing Homebrew on your server... Please wait a moment."
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-echo "Code Agency Cloud is installing Dust on your server... Please wait a moment."
+echo "Adcotech Cloud is installing Dust on your server... Please wait a moment."
 cargo install du-dust -y
-echo "Code Agency Cloud is installing CTOP on your server... Please wait a moment."
+echo "Adcotech Cloud is installing CTOP on your server... Please wait a moment."
 sudo apt install ctop -y
-echo "Your server is now ready. Welcome aboard Code Agency Cloud! Enjoy your new fast server. "
+echo "Your server is now ready. Welcome aboard Adcotech Cloud! Enjoy your new fast server. "
